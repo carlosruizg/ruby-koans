a8686f80d7f8ca2f2c02c1983177c8a42e879c9d
Carlos Ruiz <cruiz@worldremit.com>
2017-10-01 17:27:38 +0100

    Iterations

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,40 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	unless is_valid_triangle(a,b,c)
+		fail TriangleError
+	end
+	
+	return :equilateral if(a == b && a == c)		
+	return :isosceles if(a == c || b == c || a == b)	
+	:scalene
+
+	# arr = []
+	# arr << a << b << c
+	# uniqueElements = arr.uniq.size
+	# return :equilateral if uniqueElements == 1
+	# return :isosceles if uniqueElements == 2
+	# :scalene
+
+	# if(a == b)
+	# 	return :equilateral if(a == c)
+	# 	return :isosceles
+	# end	
+	# return :isosceles if(a == c || b == c)	
+	# return :scalene
+  
 end
 
+def is_valid_triangle(a,b,c)
+	if(a <= 0 || b <= 0 || c <= 0)
+		return false
+	end
+
+	if(a + b <= c || a + c <= b || c + b <= a)
+		return false
+	end
+	true
+end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
